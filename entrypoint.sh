2a6b1f27d5e2ee97fbb98416dc33fba74931ebc1
Kelvin Wong <kelvinwonggg@gmail.com>
2021-04-19 12:34:03 +0800

    test docker actions

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -l
 
-: ${$INPUT_WPE_ENV_NAME?Required WPE site name not set.}
-: ${$INPUT_WPE_SSH_KEY_PUBLIC?Required public key secret not set.}
-: ${$INPUT_WPE_SSH_KEY_PRIVATE?Required private key secret not set.}
+: ${INPUT_WPE_ENV_NAME?Required WPE site name not set.}
+: ${INPUT_WPE_SSH_KEY_PUBLIC?Required public key secret not set.}
+: ${INPUT_WPE_SSH_KEY_PRIVATE?Required private key secret not set.}
 
 echo "Hello $1"
 time=$(date)
