77f998c9bc4767e64117604e67e1d12d68bd181f
Kelvin Wong <kelvinwonggg@gmail.com>
2021-04-19 12:59:16 +0800

    test docker actions

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,14 @@
 : ${INPUT_WPE_SSH_KEY_PUBLIC?Required public key secret not set.}
 : ${INPUT_WPE_SSH_KEY_PRIVATE?Required private key secret not set.}
 
-echo "Hello $1"
-time=$(date)
-echo "::set-output name=time::$time"
+# echo "Hello $1"
+# time=$(date)
+# echo "::set-output name=time::$time"
 
-echo "INPUT_WPE_ENV_NAME: $INPUT_WPE_ENV_NAME"
-echo "INPUT_WPE_SSH_KEY_PUBLIC: $INPUT_WPE_SSH_KEY_PUBLIC"
-echo "INPUT_WPE_SSH_KEY_PRIVATE: $INPUT_WPE_SSH_KEY_PRIVATE"
+# echo "INPUT_WPE_ENV_NAME: $INPUT_WPE_ENV_NAME"
+# echo "INPUT_WPE_SSH_KEY_PUBLIC: $INPUT_WPE_SSH_KEY_PUBLIC"
+# echo "INPUT_WPE_SSH_KEY_PRIVATE: $INPUT_WPE_SSH_KEY_PRIVATE"
+
+cat $INPUT_WPE_SSH_KEY_PRIVATE >> ./private_key
+tree .
+ssh ttidev@ttidev.ssh.wpengine.net -i ./private_key
