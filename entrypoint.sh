d68ab1a92fb54b6bf5cdce1676cabe247a5e7a58
Kelvin Wong <kelvinwonggg@gmail.com>
2021-04-19 16:25:28 +0800

    test docker actions

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,7 +17,7 @@ chmod 600 "$SSH_KEY_PRIVATE_PATH"
 # ssh -i ${SSH_KEY_PRIVATE_PATH} -o StrictHostKeyChecking=no ttidev@ttidev.ssh.wpengine.net
 
 # List of changed files
-GIT_DIFF_TREE=git diff-tree --no-commit-id --name-only --diff-filter=ACMRT -r $GITHUB_SHA
+GIT_DIFF_TREE=$(git diff-tree --no-commit-id --name-only --diff-filter=ACMRT -r $GITHUB_SHA)
 echo $GIT_DIFF_TREE
 
 # Set output 'changed_files'
