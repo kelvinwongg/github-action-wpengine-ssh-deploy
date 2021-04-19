ff0d17ff36aabdbb08cedfb3cf86537f4e7c80d2
Kelvin Wong <kelvinwonggg@gmail.com>
2021-04-19 15:51:41 +0800

    test docker actions

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,18 +12,18 @@ SSH_KEY_PRIVATE_PATH="$SSH_PATH/github_action"
 SSH_KEY_PUBLIC_PATH="$SSH_PATH/github_action.pub"
 
 # Setup our SSH Connection & use keys
-mkdir "$SSH_PATH"
+# mkdir "$SSH_PATH"
 # ssh-keyscan -t rsa "$WPE_SSH_HOST" >> "$KNOWN_HOSTS_PATH"
 
 #Copy Secret Keys to container
 echo "$INPUT_WPE_SSH_KEY_PRIVATE" > "$SSH_KEY_PRIVATE_PATH"
-echo "$INPUT_WPE_SSH_KEY_PUBLIC" > "$SSH_KEY_PUBLIC_PATH"
+# echo "$INPUT_WPE_SSH_KEY_PUBLIC" > "$SSH_KEY_PUBLIC_PATH"
 
 #Set Key Perms 
 # chmod 700 "$SSH_PATH"
 # chmod 644 "$KNOWN_HOSTS_PATH"
 chmod 600 "$SSH_KEY_PRIVATE_PATH"
-chmod 644 "$SSH_KEY_PUBLIC_PATH"
+# chmod 644 "$SSH_KEY_PUBLIC_PATH"
 
 # ls . -alh
 ssh -v -p 22 -i ${SSH_KEY_PRIVATE_PATH} -o StrictHostKeyChecking=no ttidev@ttidev.ssh.wpengine.net
