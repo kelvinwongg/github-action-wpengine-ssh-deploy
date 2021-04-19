6b199c67b7bf12f477357bf354f9b411358ad457
Kelvin Wong <kelvinwonggg@gmail.com>
2021-04-19 16:24:10 +0800

    test docker actions

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,7 +13,12 @@ echo "$INPUT_WPE_SSH_KEY_PRIVATE" > "$SSH_KEY_PRIVATE_PATH"
 #Set Key Perms 
 chmod 600 "$SSH_KEY_PRIVATE_PATH"
 
-# TRY SSH
-ssh -i ${SSH_KEY_PRIVATE_PATH} -o StrictHostKeyChecking=no ttidev@ttidev.ssh.wpengine.net
+# TEST SSH CONNECTION
+# ssh -i ${SSH_KEY_PRIVATE_PATH} -o StrictHostKeyChecking=no ttidev@ttidev.ssh.wpengine.net
 
-echo "::set-output name=changed_files::'This is a list of changed files.'"
+# List of changed files
+GIT_DIFF_TREE=git diff-tree --no-commit-id --name-only --diff-filter=ACMRT -r $GITHUB_SHA
+echo $GIT_DIFF_TREE
+
+# Set output 'changed_files'
+# echo "::set-output name=changed_files::'This is a list of changed files.'"
