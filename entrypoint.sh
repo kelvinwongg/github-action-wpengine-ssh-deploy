749a9151ce6122d65ade347e721968e849134729
Kelvin Wong <kelvinwonggg@gmail.com>
2021-04-19 14:52:41 +0800

    test docker actions

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,16 +4,26 @@
 : ${INPUT_WPE_SSH_KEY_PUBLIC?Required public key secret not set.}
 : ${INPUT_WPE_SSH_KEY_PRIVATE?Required private key secret not set.}
 
-# echo "Hello $1"
-# time=$(date)
-# echo "::set-output name=time::$time"
+#SSH Key Vars 
+SSH_PATH="$HOME/.ssh"
+KNOWN_HOSTS_PATH="$SSH_PATH/known_hosts"
+WPE_SSHG_KEY_PRIVATE_PATH="$SSH_PATH/github_action"
+WPE_SSHG_KEY_PUBLIC_PATH="$SSH_PATH/github_action.pub"
 
-# echo "INPUT_WPE_ENV_NAME: $INPUT_WPE_ENV_NAME"
-# echo "INPUT_WPE_SSH_KEY_PUBLIC: $INPUT_WPE_SSH_KEY_PUBLIC"
-# echo "INPUT_WPE_SSH_KEY_PRIVATE: $INPUT_WPE_SSH_KEY_PRIVATE"
+# Setup our SSH Connection & use keys
+mkdir "$SSH_PATH"
+ssh-keyscan -t rsa "$WPE_SSH_HOST" >> "$KNOWN_HOSTS_PATH"
 
-echo $INPUT_WPE_SSH_KEY_PRIVATE > ./private_key
-chmod 400 ./private_key
+#Copy Secret Keys to container
+echo "$WPE_SSHG_KEY_PRIVATE" > "$WPE_SSHG_KEY_PRIVATE_PATH"
+echo "$WPE_SSHG_KEY_PUBLIC" > "$WPE_SSHG_KEY_PUBLIC_PATH"
+
+#Set Key Perms 
+chmod 700 "$SSH_PATH"
+chmod 644 "$KNOWN_HOSTS_PATH"
+chmod 600 "$WPE_SSHG_KEY_PRIVATE_PATH"
+chmod 644 "$WPE_SSHG_KEY_PUBLIC_PATH"
+
 cat private_key
 # ls . -alh
-ssh -i "./private_key" -o "StrictHostKeyChecking no" ttidev@ttidev.ssh.wpengine.net
+ssh -v -p 22 -i ${WPE_SSHG_KEY_PRIVATE_PATH} -o StrictHostKeyChecking=no ttidev@ttidev.ssh.wpengine.net
