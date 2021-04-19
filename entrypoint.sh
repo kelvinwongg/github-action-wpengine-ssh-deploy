aa78f7d6bbc405e55197b314dd8e46475964bb0e
Kelvin Wong <kelvinwonggg@gmail.com>
2021-04-19 17:08:36 +0800

    test docker actions

--- entrypoint.sh
+++ entrypoint.sh
@@ -35,4 +35,6 @@ WPE_DESTINATION="$WPE_ENV_NAME"@"$WPE_SSH_HOST":sites/"$WPE_ENV_NAME"/"$DIR_PATH
 GIT_DIFF_TREE_PATH="./git_diff_tree"
 GIT_DIFF_TREE=$(git diff-tree --no-commit-id --name-only --diff-filter=ACMRT -r $GITHUB_SHA)
 echo "$GIT_DIFF_TREE" >> "$GIT_DIFF_TREE_PATH"
-cat $GIT_DIFF_TREE_PATH
+
+# Do the RSYNC
+rsync -azPvn --files-from=$GIT_DIFF_TREE_PATH -e "ssh -i ${SSH_KEY_PRIVATE_PATH}" $SRC_PATH "$WPE_DESTINATION"
